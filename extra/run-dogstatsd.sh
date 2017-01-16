a068fa8e80a1d4a0a1c93631352bc3ffb90c0ebc
Emre Basala <emrebasala@gmail.com>
2017-01-16 16:36:15 +0000

    added running the agent

--- extra/run-dogstatsd.sh
+++ extra/run-dogstatsd.sh
@@ -27,14 +27,13 @@ instances:
   - database_url:  postgres://${PGBOUNCER_STATS_USER}:${PGBOUNCER_STATS_PASSWORD}@127.0.0.1:6000/pgbouncer
 EOFEOF
 
-
 # Update integration directory configuration
 sed -i -e"s/^.*UNIX_CONFIG_PATH =.*$/UNIX_CONFIG_PATH = '\/app\/.apt\/opt\/datadog-agent\/agent'/" /app/.apt/opt/datadog-agent/agent/config.py
-
 
 (
   # Load our library path first when starting up
   export LD_LIBRARY_PATH=/app/.apt/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH
   mkdir -p /tmp/logs/datadog
+  exec /app/.apt/opt/datadog-agent/embedded/bin/python /app/.apt/opt/datadog-agent/agent/agent.py start
   exec /app/.apt/opt/datadog-agent/embedded/bin/python /app/.apt/opt/datadog-agent/agent/dogstatsd.py start
 )
