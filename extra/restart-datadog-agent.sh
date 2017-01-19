77178db216d19f332ac6a07fbd1e90f9dbfb7552
Emre Basala <emrebasala@gmail.com>
2017-01-19 11:51:09 +0000

    Removed setting the conf.d directory- seems to work without it

--- extra/restart-datadog-agent.sh
+++ extra/restart-datadog-agent.sh
@@ -5,9 +5,6 @@ if [[ $DISABLE_DATADOG_AGENT ]]; then
   exit 0
 fi
 
-# Update integration directory configuration
-sed -i -e"s/^.*UNIX_CONFIG_PATH =.*$/UNIX_CONFIG_PATH = '\/app\/.apt\/opt\/datadog-agent\/agent'/" /app/.apt/opt/datadog-agent/agent/config.py
-
 (
   # Load our library path first when starting up
   export LD_LIBRARY_PATH=/app/.apt/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH
